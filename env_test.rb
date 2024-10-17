3245fa32e01a63c70962b8eaaba089c25cc414b6
jackymiranda <jackelyn.miranda@chicagobooth.edu>
2024-10-17 00:13:15 +0000

    2

---
+++ env_test.rb
@@ -0,0 +1,11 @@
+# require "dotenv/load"
+
+# pp ENV.fetch("GMAPS_KEY")
+# pp ENV.fetch("OPENAI_KEY")
+
+#gem install http (si tienes gemfile no necesitas instalarlo ahora)
+
+require "http"
+require "json"
+require "dotenv/load"
+
